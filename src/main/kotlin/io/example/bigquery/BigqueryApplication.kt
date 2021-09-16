eb4508ea63449181bf9c80fac405f5e1d07dadc8
choy-ys <project.log.062@gmail.com>
2021-09-16 13:18:54 +0900

    [2021-09-16]용석:Initial commit

---
+++ src/main/kotlin/io/example/bigquery/BigqueryApplication.kt
@@ -0,0 +1,11 @@
+package io.example.bigquery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BigqueryApplication
+
+fun main(args: Array<String>) {
+    runApplication<BigqueryApplication>(*args)
+}
---
+++ src/test/kotlin/io/example/bigquery/BigqueryApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.example.bigquery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BigqueryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
